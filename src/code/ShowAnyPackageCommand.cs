056b4d805ce7000911925049b19623ba7c3efb3d
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2024-09-12 20:43:04 -0500

    Fix formatting (#6)

--- src/code/ShowAnyPackageCommand.cs
+++ src/code/ShowAnyPackageCommand.cs
@@ -2,24 +2,23 @@ using System.Management.Automation;
 using Terminal.Gui;
 using AnyPackage.Tui;
 
-namespace AnyPackage.Commands
-{
-    [Cmdlet("Show", "AnyPackage")]
-    public class ShowAnyPackageCommand : PSCmdlet
-    {
-        protected override void BeginProcessing()
-        {
-            Application.Init();
-            Application.QuitKey = Key.Esc;
+namespace AnyPackage.Commands;
 
-            try
-            {
-                Application.Run<AnyPackageView>();
-            }
-            finally
-            {
-                Application.Shutdown();
-            }
+[Cmdlet("Show", "AnyPackage")]
+public class ShowAnyPackageCommand : PSCmdlet
+{
+    protected override void BeginProcessing()
+    {
+        Application.Init();
+        Application.QuitKey = Key.Esc;
+
+        try
+        {
+            Application.Run<AnyPackageView>();
+        }
+        finally
+        {
+            Application.Shutdown();
         }
     }
 }
